56c1d80a8c0ce4a0693d37621fc85f4bdbd85357
DevNugget <84568105+DevNugget@users.noreply.github.com>
2021-06-15 11:07:56 +0530

    Add files via upload
    Script Template

---
+++ commented_empty_++.gd
@@ -0,0 +1,28 @@
+extends %BASE%
+#class_name {Class Name}
+
+
+# Script Template by DevNugget;
+
+
+## Variables ::
+# :: (Int)/s;
+# :: (Str)/s;
+# :: (Bool)/s;
+# :: (Float)/s;
+# :: (Export/Onready)/s;
+# :: (Other)/s;
+
+
+## Functions ::
+# :: Built-in functions;
+
+
+func _ready()%VOID_RETURN%:
+    pass # Replace with code;
+
+
+# :: Self-assigned functions;
+
+
+# :: Signal functions;
